198f087a2c6d825fc922165cf5faa5eacf01107b
Max Lysenko <addition.mail.address@gmail.com>
2020-03-10 14:00:25 +0200

    Fixed promise handling.

--- src/asyncStorageBackend.js
+++ src/asyncStorageBackend.js
@@ -4,7 +4,6 @@ import {getStorageKey, getVersion} from "./helpers";
 const defaultBackendOptions = {
 	prefix: "i18next_res_",
 	expirationTime: 7 * 24 * 60 * 60 * 1000,
-	defaultVersion: undefined,
 	versions: {},
 };
 
@@ -13,26 +12,31 @@ function AsyncStorageBackend() {}
 AsyncStorageBackend.type = "backend";
 
 AsyncStorageBackend.prototype.init = function(services, backendOptions = {}) {
+	this.services = services;
 	this.options = {...defaultBackendOptions, ...backendOptions};
 };
 
 AsyncStorageBackend.prototype.read = function(language, namespace, callback) {
 	const now = Date.now();
-	const resource = AsyncStorage.getItem(getStorageKey(this.options.prefix, language, namespace));
 
-	if (!resource) {
-		return callback(null, null);
-	}
+	AsyncStorage.getItem(getStorageKey(this.options.prefix, language, namespace))
+		.then(resource => {
+			if (resource) {
+				const version = getVersion(this.options, language);
+				const parsedResource = JSON.parse(resource);
 
-	const version = getVersion(this.options, language);
-	const parsedResource = JSON.parse(resource);
+				if (parsedResource.i18nStamp && parsedResource.i18nStamp + this.options.expirationTime > now && version === parsedResource.i18nVersion) {
+					delete parsedResource.i18nVersion;
+					delete parsedResource.i18nStamp;
 
-	if (parsedResource.i18nStamp && parsedResource.i18nStamp + this.options.expirationTime > now && version === parsedResource.i18nVersion) {
-		delete parsedResource.i18nVersion;
-		delete parsedResource.i18nStamp;
+					return callback(null, parsedResource);
+				}
+			}
 
-		return callback(null, parsedResource);
-	}
+			callback(null, null);
+		}).catch(e => {
+			callback(e, null);
+		});
 
 	return callback(null, null);
 };
